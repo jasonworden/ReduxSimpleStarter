fc766d71ea4b98e5572367ffb1c62451b8e1a8a9
Jason Worden <wordenjasonp@gmail.com>
2016-09-21 17:42:56 -0700

    Created SearchBar component class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyAkULVvoDtRA45aPYrUFj40knzLfymBp3Y';
 
 //create component
 const App = () => {
-  return <div>Hiiiiii!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 //put component's HTML in DOM

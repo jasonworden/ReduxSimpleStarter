4a7fd5074761c636c9234a1f3cf17f573d5dc408
Jason Worden <wordenjasonp@gmail.com>
2016-09-21 19:16:59 -0700

    shows length of list of videos received from youtube search api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.setState = this.setState.bind(this);
+
+    this.state = {term: ''};
+  }
+
   render() {
-    return <input />
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>
+        <div>
+          Search term: {this.state.term}
+        </div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAkULVvoDtRA45aPYrUFj40knzLfymBp3Y';
 
 //create component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const that = this;
+    this.setState = this.setState.bind(this);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'pizza'}, function(videos) {
+      that.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>app:</h1>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 //put component's HTML in DOM
